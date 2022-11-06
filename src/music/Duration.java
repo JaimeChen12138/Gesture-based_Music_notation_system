7df0e8ac865bba7107840f86976e88613584f631
yizhouchen <joe1542827496@gmail.com>
2022-11-06 01:44:47 -0700

    "new commit"

---
+++ src/music/Duration.java
@@ -0,0 +1,26 @@
+package music;
+
+import reaction.Mass;
+
+import java.awt.*;
+
+public abstract class Duration extends Mass {
+    public int nFlag = 0, nDot = 0;
+    public Duration(){
+        super("NOTE");
+    }
+
+    public abstract void show(Graphics g); // children of duration implement it
+
+    public void incFlag() {if (nFlag < 4) {nFlag++;}}
+    public void decFlag() {if (nFlag > -2) {nFlag--;}}
+
+    public void cycleDots(){
+        nDot++;
+        if (nDot > 3){
+            nDot = 0;
+        }
+    }
+
+
+}
